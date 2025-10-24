5298a81a3598c0c25dd00250960f9806389ced5b
devedge <richard.pjski2@proton.me>
2025-10-23 21:17:38 -0400

    Modularized initscripts

--- init-intermediate-ca-directory.sh
+++ init-intermediate-ca-directory.sh
@@ -1,2 +1,6 @@
-mkdir -p intermediate-ca/{certs,crl,csr,newcerts,private}
-chmod 700 intermediate-ca/private
+INTERMEDIATE_CA_PATH="intermediate-ca"
+mkdir -p ${INTERMEDIATE_CA_PATH}/{certs,crl,csr,newcerts,private}
+chmod 700 ${INTERMEDIATE_CA_PATH}/private
+touch ${INTERMEDIATE_CA_PATH}/index.txt
+echo 1000 > ${INTERMEDIATE_CA_PATH}/serial
+echo 1000 > ${INTERMEDIATE_CA_PATH}/crlnumber
--- init-root-ca-directory.sh
+++ init-root-ca-directory.sh
@@ -1,2 +1,5 @@
-mkdir -p root-ca/{certs,crl,newcerts,private}
-chmod 700 root-ca/private
+ROOT_CA_PATH="root-ca"
+mkdir -p ${ROOT_CA_PATH}/{certs,crl,newcerts,private}
+chmod 700 ${ROOT_CA_PATH}/private
+touch ${ROOT_CA_PATH}/index.txt
+echo 1000 > ${ROOT_CA_PATH}/serial
