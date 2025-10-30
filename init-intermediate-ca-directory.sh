153ea2e78bc77594fffa3cad732a0af5b6cace28
devedge <richard.pjski2@proton.me>
2025-10-30 12:40:32 -0400

    Removed quotes, which broke shell expansion

--- init-intermediate-ca-directory.sh
+++ init-intermediate-ca-directory.sh
@@ -1,15 +1,15 @@
 # Type the following before running:
 # export INTERMEDIATE_CA_NAME="intermediate-ca"
 
-mkdir -p "${INTERMEDIATE_CA_NAME}/{certs,crl,csr,newcerts,private}"
-chmod 700 "${INTERMEDIATE_CA_NAME}/private"
-touch "${INTERMEDIATE_CA_NAME}/index.txt"
+mkdir -p ${INTERMEDIATE_CA_NAME}/{certs,crl,csr,newcerts,private}
+chmod 700 ${INTERMEDIATE_CA_NAME}/private
+touch ${INTERMEDIATE_CA_NAME}/index.txt
 
 # Prevent accidentally overwriting the serial or crlnumber files
-if [ ! -f "${INTERMEDIATE_CA_NAME}/serial" ]; then
-    echo 1000 > "${INTERMEDIATE_CA_NAME}/serial"
+if [ ! -f ${INTERMEDIATE_CA_NAME}/serial ]; then
+    echo 1000 > ${INTERMEDIATE_CA_NAME}/serial
 fi
 
-if [ ! -f "${INTERMEDIATE_CA_NAME}/crlnumber" ]; then
-    echo 1000 > "${INTERMEDIATE_CA_NAME}/crlnumber"
+if [ ! -f ${INTERMEDIATE_CA_NAME}/crlnumber ]; then
+    echo 1000 > ${INTERMEDIATE_CA_NAME}/crlnumber
 fi
--- init-root-ca-directory.sh
+++ init-root-ca-directory.sh
@@ -1,11 +1,11 @@
 # Type the following before running:
 # export ROOT_CA_NAME="root-ca"
 
-mkdir -p "${ROOT_CA_NAME}/{certs,crl,newcerts,private}"
-chmod 700 "${ROOT_CA_NAME}/private"
-touch "${ROOT_CA_NAME}/index.txt"
+mkdir -p ${ROOT_CA_NAME}/{certs,crl,newcerts,private}
+chmod 700 ${ROOT_CA_NAME}/private
+touch ${ROOT_CA_NAME}/index.txt
 
 # Prevent accidentally overwriting the serial file
-if [ ! -f "${ROOT_CA_NAME}/serial" ]; then
-    echo 1000 > "${ROOT_CA_NAME}/serial"
+if [ ! -f ${ROOT_CA_NAME}/serial ]; then
+    echo 1000 > ${ROOT_CA_NAME}/serial
 fi
